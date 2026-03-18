48f5e4030bef012280f5d788ab0284c03143279c
leitoland95 <leitoland95@gmail.com>
2026-03-17 21:11:16 -0400

    Actualizar main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+###### DEPENDENCIAS ##############
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
 import os, requests, threading
 from openai import OpenAI
+import uvicorn
+
+###### VARIABLES #################
 
 API_URL = "https://api.groq.com/openai/v1"
 MODEL_NAME = "openai/gpt-oss-20b"   # ajusta al modelo disponible en Groq
@@ -12,10 +17,44 @@ client_ia = OpenAI(api_key=API_KEY, base_url=API_URL)
 
 app = FastAPI()
 
+exec_logs = []
+
+###### CLASES ####################
+
 class ChatRequest(BaseModel):
     prompt: str
     image_base64: Optional[str] = None   # ðŸ‘ˆ aquÃ­ recibimos la cadena Base64
+    
+    
+###### FUNCIONES #################
 
+def log(msg: str):
+    exec_logs.append(msg)
+    print(msg)
+    
+def keep_alive():
+    url = "https://grok-gpt.onrender.com"
+    if not url:
+        log("No se encontró RENDEREXTERNALURL, keep_alive desactivado")
+        return
+    while True:
+        try:
+            requests.get(url, timeout=10)
+            log(f"Ping a {url} para mantener vivo el servicio")
+        except Exception as e:
+            log(f"Error en keep_alive: {e}")
+        time.sleep(60)
+        
+###### ENDPOINTS #################
+
+@app.get("/")
+def root():
+    return {"status":200}
+    
+@app.get("/logs")
+def save_logs():
+    return exec_logs    
+    
 @app.post("/chat")
 async def chat_endpoint(body: ChatRequest):
     try:
@@ -39,18 +78,7 @@ async def chat_endpoint(body: ChatRequest):
         return {"error": str(e)}
         
         
-def keep_alive():
-    url = "https://grok-gpt.onrender.com"
-    if not url:
-        log("No se encontró RENDEREXTERNALURL, keep_alive desactivado")
-        return
-    while True:
-        try:
-            requests.get(url, timeout=10)
-            log(f"Ping a {url} para mantener vivo el servicio")
-        except Exception as e:
-            log(f"Error en keep_alive: {e}")
-        time.sleep(60)
+###### EJECUCIÓN #################
 
 threading.Thread(target=keep_alive, daemon=True).start()
 
